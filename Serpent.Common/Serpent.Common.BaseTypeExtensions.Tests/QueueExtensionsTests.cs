885afad916b8d273f2ee1c0afe8b1a59758698be
Erik Bergman <erik_r_bergman@hotmail.com>
2018-02-27 10:49:05 +0100

    Added queue extensions with tests

---
+++ Serpent.Common/Serpent.Common.BaseTypeExtensions.Tests/QueueExtensionsTests.cs
@@ -0,0 +1,78 @@
+﻿namespace Serpent.Common.BaseTypeExtensions.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Serpent.Common.BaseTypeExtensions.Collections;
+
+    [TestClass]
+    public class QueueExtensionsTests
+    {
+        [TestMethod]
+        public void ToQueueTest()
+        {
+            var items = new[] { 1, 2, 3, 4, 5, 6 };
+
+            var queue = items.ToQueue();
+
+            Assert.IsNotNull(queue);
+
+            Assert.AreEqual(items.Length, queue.Count);
+
+            foreach (var item in items)
+            {
+                var queueItem = queue.Dequeue();
+                Assert.AreEqual(item, queueItem);
+            }
+        }
+
+        [TestMethod]
+        public void EnqueueRangeTest()
+        {
+            var items = new[] { 1, 2, 3, 4, 5, 6 };
+
+            var queue = new Queue<int>();
+
+            queue.EnqueueRange(items);
+
+            Assert.AreEqual(items.Length, queue.Count);
+
+            foreach (var item in items)
+            {
+                var queueItem = queue.Dequeue();
+                Assert.AreEqual(item, queueItem);
+            }
+        }
+
+        [TestMethod]
+        public void DequeueManyTest()
+        {
+            var items = new[] { 1, 2, 3, 4, 5, 6 };
+
+            var queue = new Queue<int>();
+
+            queue.EnqueueRange(items);
+
+            var dequeued = queue.DequeueMany(3).ToArray();
+
+            Assert.AreEqual(3, dequeued.Length);
+
+            Assert.AreEqual(1, dequeued[0]);
+            Assert.AreEqual(2, dequeued[1]);
+            Assert.AreEqual(3, dequeued[2]);
+
+            dequeued = queue.DequeueMany(6).ToArray();
+            Assert.AreEqual(3, dequeued.Length);
+
+            Assert.AreEqual(0, queue.Count);
+
+            Assert.AreEqual(4, dequeued[0]);
+            Assert.AreEqual(5, dequeued[1]);
+            Assert.AreEqual(6, dequeued[2]);
+
+        }
+
+    }
+}
---
+++ Serpent.Common/Serpent.Common.BaseTypeExtensions/Collections/QueueExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serpent.Common.BaseTypeExtensions.Collections
+{
+    public static class QueueExtensions
+    {
+        public static Queue<T> EnqueueRange<T>(this Queue<T> queue, IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                queue.Enqueue(item);
+            }
+
+            return queue;
+        }
+
+        public static Queue<T> ToQueue<T>(IEnumerable<T> collection)
+        {
+            return new Queue<T>(collection);
+        }
+
+        public static IEnumerable<T> DequeueMany<T>(this Queue<T> queue, int count)
+        {
+            var result = new List<T>(Math.Min(count, queue.Count));
+
+            for (int i = 0; i < count; i++)
+            {
+                if (queue.Count > 0)
+                {
+                    result.Add(queue.Dequeue());
+                }
+            }
+
+            return result;
+        }
+    }
+}
