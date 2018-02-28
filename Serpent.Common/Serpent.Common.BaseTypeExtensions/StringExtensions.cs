309ccee52bf6c4974900c6eb5259ed74dae4e316
Erik Bergman <erik_r_bergman@hotmail.com>
2018-02-28 22:40:42 +0100

    Added a ToDecimalOrDefault string extension

---
+++ Serpent.Common/Serpent.Common.BaseTypeExtensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Serpent.Common.BaseTypeExtensions
+{
+    public static class StringExtensions
+    {
+        public static decimal ToDecimalOrDefault(this string text, decimal defaultValue)
+        {
+            if (string.IsNullOrWhiteSpace(text) || decimal.TryParse(text, out var value) == false)
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
+    }
+}
