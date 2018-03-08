e09a0e384ad820abb3275c73c99e832d52fac0e1
Erik Bergman <erik_r_bergman@hotmail.com>
2018-03-08 10:01:44 +0100

    Added conditional ToInt*, ToDecimal* and ToTimeSpan*

--- Serpent.Common/Serpent.Common.BaseTypeExtensions/StringExtensions.cs
+++ Serpent.Common/Serpent.Common.BaseTypeExtensions/StringExtensions.cs
@@ -14,6 +14,16 @@
             return value;
         }
 
+        public static decimal ToDecimalOrDefault(this string input, Func<decimal, bool> predicate, decimal defaultValue)
+        {
+            if (decimal.TryParse(input, out var value) == false || predicate(value) == false)
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
+
         public static decimal? ToDecimalOrDefaultNullable(this string input, decimal? defaultValue = null)
         {
             if (decimal.TryParse(input, out var value) == false)
@@ -24,10 +34,29 @@
             return value;
         }
 
+        public static decimal? ToDecimalOrDefaultNullable(this string input, Func<decimal, bool> predicate, decimal? defaultValue = null)
+        {
+            if (decimal.TryParse(input, out var value) == false || predicate(value) == false)
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
 
         public static int ToIntOrDefault(this string input, int defaultValue)
         {
             if (int.TryParse(input, out var value) == false)
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
+
+        public static int ToIntOrDefault(this string input, Func<int, bool> predicate, int defaultValue)
+        {
+            if (int.TryParse(input, out var value) == false || predicate(value) == false)
             {
                 return defaultValue;
             }
@@ -45,11 +74,41 @@
             return value;
         }
 
+        public static int? ToIntOrDefaultNullable(this string input, Func<int, bool> predicate, int? defaultValue = null)
+        {
+            if (int.TryParse(input, out var value) == false || predicate(value) == false)
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
+
+        public static TimeSpan ToTimeSpanOrDefault(this string input, Func<TimeSpan, bool> predicate, TimeSpan defaultValue)
+        {
+            if (TimeSpan.TryParse(input, out var value) || predicate(value) == false)
+            {
+                return value;
+            }
+
+            return defaultValue;
+        }
+
         public static TimeSpan ToTimeSpanOrDefault(this string input, TimeSpan defaultValue)
         {
-            if (TimeSpan.TryParse(input, out var result))
+            if (TimeSpan.TryParse(input, out var value))
             {
-                return result;
+                return value;
+            }
+
+            return defaultValue;
+        }
+
+        public static TimeSpan? ToTimeSpanOrDefaultNullable(this string input, Func<TimeSpan, bool> predicate, TimeSpan? defaultValue)
+        {
+            if (TimeSpan.TryParse(input, out var value) || predicate(value) == false)
+            {
+                return value;
             }
 
             return defaultValue;
