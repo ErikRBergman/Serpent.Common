cb93531656346101977f9c88c1c40bd3e24dae93
Erik Bergman <erik_r_bergman@hotmail.com>
2018-03-08 09:56:30 +0100

    Added ToTimeSpanOrDefault & ToTimeSpanOrDefaultNullable and ToIntOrDefault and ToIntOrDefaultNullable

--- Serpent.Common/Serpent.Common.BaseTypeExtensions/StringExtensions.cs
+++ Serpent.Common/Serpent.Common.BaseTypeExtensions/StringExtensions.cs
@@ -1,10 +1,12 @@
 ﻿namespace Serpent.Common.BaseTypeExtensions
 {
+    using System;
+
     public static class StringExtensions
     {
-        public static decimal ToDecimalOrDefault(this string text, decimal defaultValue)
+        public static decimal ToDecimalOrDefault(this string input, decimal defaultValue)
         {
-            if (string.IsNullOrWhiteSpace(text) || decimal.TryParse(text, out var value) == false)
+            if (decimal.TryParse(input, out var value) == false)
             {
                 return defaultValue;
             }
@@ -12,14 +14,55 @@
             return value;
         }
 
-        public static decimal? ToDecimalOrDefaultNullable(this string text, decimal? defaultValue = null)
+        public static decimal? ToDecimalOrDefaultNullable(this string input, decimal? defaultValue = null)
         {
-            if (string.IsNullOrWhiteSpace(text) || decimal.TryParse(text, out var value) == false)
+            if (decimal.TryParse(input, out var value) == false)
             {
                 return defaultValue;
             }
 
             return value;
+        }
+
+
+        public static int ToIntOrDefault(this string input, int defaultValue)
+        {
+            if (int.TryParse(input, out var value) == false)
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
+
+        public static int? ToIntOrDefaultNullable(this string input, int? defaultValue = null)
+        {
+            if (int.TryParse(input, out var value) == false)
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
+
+        public static TimeSpan ToTimeSpanOrDefault(this string input, TimeSpan defaultValue)
+        {
+            if (TimeSpan.TryParse(input, out var result))
+            {
+                return result;
+            }
+
+            return defaultValue;
+        }
+
+        public static TimeSpan? ToTimeSpanOrDefaultNullable(this string input, TimeSpan? defaultValue)
+        {
+            if (TimeSpan.TryParse(input, out var result))
+            {
+                return result;
+            }
+
+            return defaultValue;
         }
     }
 }
