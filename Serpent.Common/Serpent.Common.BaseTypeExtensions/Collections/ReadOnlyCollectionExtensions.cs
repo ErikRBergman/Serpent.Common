596fcc233146fa6ceda9f067c36e905f79a8acb1
Erik Bergman <erik_r_bergman@hotmail.com>
2018-03-26 10:55:07 +0200

    Added a ReadOnlyCollection.ForEach and created a new Serpent.ServiceFabric.Common project. Both published to nuget

--- Serpent.Common/Serpent.Common.BaseTypeExtensions/Collections/ReadOnlyCollectionExtensions.cs
+++ Serpent.Common/Serpent.Common.BaseTypeExtensions/Collections/ReadOnlyCollectionExtensions.cs
@@ -1,6 +1,18 @@
 ﻿namespace Serpent.Common.BaseTypeExtensions.Collections
 {
+    using System;
+    using System.Collections.Generic;
+
     public static class ReadOnlyCollectionExtensions
     {
+        public static IReadOnlyCollection<T> ForEach<T>(this IReadOnlyCollection<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+            {
+                action(item);
+            }
+
+            return items;
+        }
     }
 }
---
+++ Serpent.Common/Serpent.ServiceFabric.Common/Extensions/ConditionalValueExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Serpent.ServiceFabric.Common.Extensions
+{
+    using Microsoft.ServiceFabric.Data;
+
+    public static class ConditionalValueExtensions
+    {
+        public static TValue GetValueOrDefault<TValue>(this ConditionalValue<TValue> conditionalValue, TValue defaultValue)
+        {
+            return conditionalValue.HasValue ? conditionalValue.Value : defaultValue;
+        }
+    }
+}
